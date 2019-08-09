d8ee1b294a162b1ba4e451a1f391ce26756a09af
Jim Doyle <indellibleink@gmail.com>
2019-08-08 20:00:23 -0400

    Add project files.

---
+++ HackerRank/30DaysOfCode/Day0_HelloWold.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank._30DaysOfCode
+{
+   public class Day0_HelloWold
+    {
+        public static void HelloWorld()
+        {
+            // Declare a variable named 'inputString' to hold our input.
+            String inputString;
+
+            // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+            inputString = Console.ReadLine();
+            string input_string = inputString;
+            // Print a string literal saying "Hello, World." to stdout using cout.
+            Console.WriteLine("Hello, World.");
+            Console.WriteLine(input_string);
+
+        }
+    }
+}
---
+++ HackerRank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using HackerRank._30DaysOfCode;
+namespace HackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day0_HelloWold.HelloWorld();
+        }
+    }
+}
