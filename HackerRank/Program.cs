0ea2dd2fb6904bd3719c4258bbf43ee7b1efe49d
Jim Doyle <indellibleink@gmail.com>
2019-08-08 21:10:01 -0400

    added Day 1

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 using HackerRank._30DaysOfCode;
 namespace HackerRank
 {
@@ -7,6 +19,7 @@ namespace HackerRank
         static void Main(string[] args)
         {
             Day0_HelloWold.HelloWorld();
+            Day1_Data_Types.DataTypes();
         }
     }
 }
