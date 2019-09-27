eac4cb5822aa55cdadd68e88c21a0ff2cdf1abbf
Jim Doyle <indellibleink@gmail.com>
2019-09-26 23:31:08 -0400

    Completed BirthdayCandles

---
+++ HackerRank/Practise/Algorithms/Warmups/BirthdayCandles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.Practise.Algorithms.Warmups
+{
+    class BirthdayCandles
+    {
+        public static void birthdayCandles()
+        {
+            int count = 1;
+            Array.Sort(ar);
+            Array.Reverse(ar);
+            if (ar[0] == ar[ar.Length - 1])
+            {
+                count = ar.Length;
+                return count;
+            }
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] == ar[i + 1])
+                    count++;
+                else
+                    break;
+            }
+            return count;
+        }
+    }
+}
