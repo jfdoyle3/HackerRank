db00de22901a5ba8ce925f28fb197dcccf2f5b8c
Jim Doyle <indellibleink@gmail.com>
2020-05-21 13:05:49 -0400

    removed project and left solutions

--- dotNet/HackerRank/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "ET002:Namespace does not match file path or default namespace", Justification = "<Pending>", Scope = "namespace", Target = "~N:HackerRank._30DaysOfCode")]
-
