3365ab1cb86b1bbb072d84de076e731db3064413
Jim Doyle <indellibleink@gmail.com>
2020-09-22 17:11:36 -0400

    Working on Counting Valleys

--- java/InteviewPrep/Warm-up_Challenges/Counting Valleys.java
+++ java/InteviewPrep/Warm-up_Challenges/Counting Valleys.java
@@ -12,15 +12,6 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'countingValleys' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts following parameters:
-     *  1. INTEGER steps
-     *  2. STRING path
-     */
-
     public static int countingValleys(int steps, String path) {
     String[] aPath=path.split("");
     int seaLevel=0;
@@ -59,8 +50,7 @@ class Result {
     }
  //  System.out.println("result: "+counter);
     return counter-1;
-    } // end ----- 
-    // methods---------
+    } 
 }
 public class Solution {
     public static void main(String[] args) throws IOException {
