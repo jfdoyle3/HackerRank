b7dc7d5fa7d2aaf507a07ad8baab91c81dcd633a
jfdoyle3 <indellibleink@gmail.com>
2020-10-12 18:18:16 -0400

    Have New Year Chaos working need better Sorting.

--- java/InteviewPrep/Arrays/New Year Chaos.java
+++ java/InteviewPrep/Arrays/New Year Chaos.java
@@ -10,36 +10,42 @@ public class Solution {
 
     // Complete the minimumBribes function below.
     static void minimumBribes(int[] q) {
-      //  System.out.println("fi: "+findIndex(q,1));
-       // int[] sorted=q;
-      //  Arrays.sort(sorted);
-       // System.out.println("fis: "+findIndex(sorted,1));
-       for (int idx=0; idx<q.length; idx++){
-         int location=findIndex(q, q[idx]);
-           System.out.println("q: "+q[idx]+" | l: "+location);
-       }
-
-        // get element location from sorted and compare to q ;
-        // subtract to get distsnce to accual location.
-        // greater than 3 to chaotic .. less than 3 add to bribeCounter.
-        // ex. sorted 1 pos: 0   q: 1 pos: 1  1-0=1; bribeCounter=1;
-        //     sorted 5 pos: 4   q: 5 pos: 2  4-2=2; bribeCounter=2+1=3
-        //  q: 2 pos: 0  sorted: 2 pos: 1
-        // 2 > 1 find where 1 started from sort
-        // need to figure how to skip over the checked elements.
-        // 
-    }
-    static int findIndex(int[] array,int element){
-     //  System.out.println("Find Index");
-          int arrayIndex=-1;
-       for(int idx = 0; idx < array.length; idx++) {
-            if(array[idx] == element) {
-                arrayIndex = idx;
-                break;
+        HashMap<Integer, Integer> hm = new HashMap<>(); 
+       int bribeCounter = 0;
+       int[] arr = q; 
+       int n = arr.length;
+       int temp = 0; 
+       List<Integer> changes = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j-1] > arr[j]) {
+                    bribeCounter++; 
+                    temp = arr[j-1]; 
+                    // System.out.println(temp); 
+                    arr[j-1] = arr[j];
+                    arr[j] = temp; 
+                    changes.add(temp);
+                }
+            }  
+        } 
+            for (int num : changes){
+                if(hm.containsKey(num)){
+                hm.put(num, hm.get(num)+1);
+                } else {
+                hm.put(num,1);
+                }
             }
-        }
-        return arrayIndex;
-    }
+            int max = Collections.max(hm.values());
+                
+                    if (max > 2) {
+                        System.out.println("Too chaotic"); 
+                    } else {
+                        System.out.println(bribeCounter); 
+                    }
+ 
+    } 
+    
+
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -59,10 +65,11 @@ public class Solution {
                 int qItem = Integer.parseInt(qItems[i]);
                 q[i] = qItem;
             }
-
-            minimumBribes(q);
+         //   System.out.print("test case |  ");
+            minimumBribes(q); 
+            
         }
-
+       
         scanner.close();
     }
 }
---
+++ java/InteviewPrep/Arrays/New Year ChaosAlt.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+        sort(q);
+        for (int w : q){
+        System.out.print(w+" ");
+        }
+    }
+   
+    static void sort(int arr[]) 
+    { 
+        int n = arr.length; 
+  
+        // One by one move boundary of unsorted subarray 
+        for (int i = 0; i < n-1; i++) 
+        { 
+            // Find the minimum element in unsorted array 
+            int min_idx = i; 
+            for (int j = i+1; j < n; j++) 
+                if (arr[j] < arr[min_idx]) 
+                    min_idx = j; 
+  
+            // Swap the found minimum element with the first 
+            // element 
+            int temp = arr[min_idx]; 
+            arr[min_idx] = arr[i]; 
+            arr[i] = temp; 
+        } 
+    } 
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] q = new int[n];
+
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+            }
+            
+            minimumBribes(q);
+            System.out.println(">>>-------> Test Case <-----------<<<");
+        }
+
+        scanner.close();
+    }
+}
