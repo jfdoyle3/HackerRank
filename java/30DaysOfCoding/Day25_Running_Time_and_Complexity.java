653471ca0380eb1237123540e15a213789e85970
Jim Doyle <indellibleink@gmail.com>
2020-11-02 16:09:22 -0500

    Day 25 all but one test cases passes.

--- java/30DaysOfCoding/Day25_Running_Time_and_Complexity.java
+++ java/30DaysOfCoding/Day25_Running_Time_and_Complexity.java
@@ -3,19 +3,30 @@ import java.util.*;
 
 public class Solution {
 
+    void isPrime(int number){
+
+    }
+
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-
+        
         int number=input.nextInt();
-
         for (int idx=0; idx<number; idx++){
+            int count=0;
             int testNumber=input.nextInt();
-          //  System.out.println(testNumber);
-            if (testNumber%2==1){
-                System.out.println("Prime");
-            } else {
-                System.out.println("Not prime");
-            }
+            for (int idx2=2; idx2<=testNumber; idx2++){
+                int result=testNumber%idx2;
+                if(result==0){
+                    count++;
+                }
+             
+            } 
+            if (count==1){
+                    System.out.println("Prime");
+                } else {
+                    System.out.println("Not prime");
+                }
+         
         }
 
     }
