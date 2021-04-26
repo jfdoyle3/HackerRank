20767506fe04a72fac7b8c34ccecfe2bf659246e
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-04-26 00:48:40 -0400

    Organizing Balls have some cases pass

--- java/Practice/Algorithms/Implementation/Organizing_Containers_of_Balls.java
+++ java/Practice/Algorithms/Implementation/Organizing_Containers_of_Balls.java
@@ -4,66 +4,95 @@ import java.security.*;
 import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.function.*;
 import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class Solution {
+class Result {
 
-    // Complete the organizingContainers function below.
-    static String organizingContainers(int[][] container) {
-        System.out.println("Length of container: "+container.length);
-        int testCases=container.length;
+    /*
+     * Complete the 'organizingContainers' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts 2D_INTEGER_ARRAY container as parameter.
+     */
+
+    public static String organizingContainers(List<List<Integer>> container) {
+        String result="";
         
-        String result="impossible";
-        HashMap<Integer,Integer> bins=new HashMap<>();
-        List <Integer> rows=new ArrayList<>();
-        List <Integer> columns=new ArrayList<>();
+        List<Integer> bins=new ArrayList<>();
+        List<Integer> balls=new ArrayList<>();
         int rowTotal=0;
-        int rowLen=container[0].length;
-        int colLen=container.length;
+        int colTotal=0;
+        int rowLen=container.get(0).size();
+        int colLen=container.size();
         
-        for(int col=0; col<colLen; col++){
-        for(int row=0; row<rowLen; row++){
-          //   rowTotal=rowTotal+container[1][idx];
-              System.out.printf("%d ",container[col][row]); 
-      //  rows.add(rowTotal);
+        do{
+            for(int idx=0; idx<rowLen; idx++)
+                rowTotal+=container.get(colLen-1).get(idx);
+                
+            bins.add(rowTotal);
+            rowTotal=0;
+            colLen--;
+        }while(colLen>0);
         
+        do{
+            for(int col=0; col<container.size(); col++)
+                colTotal+=container.get(col).get(rowLen-1);
+        
+        balls.add(colTotal);
+        colTotal=0;
+        rowLen--;
+        }while(rowLen>0);
+        
+        if(bins.equals(balls)){
+            result="Possible";
+        } else {
+            result="Impossible";
         }
-        }
+        
         return result;
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
+}
 
+public class Solution {
     public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int q = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
 
-        for (int qItr = 0; qItr < q; qItr++) {
-            int n = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-            int[][] container = new int[n][n];
+                List<List<Integer>> container = new ArrayList<>();
 
-            for (int i = 0; i < n; i++) {
-                String[] containerRowItems = scanner.nextLine().split(" ");
-                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+                IntStream.range(0, n).forEach(i -> {
+                    try {
+                        container.add(
+                            Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                        );
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
 
-                for (int j = 0; j < n; j++) {
-                    int containerItem = Integer.parseInt(containerRowItems[j]);
-                    container[i][j] = containerItem;
-                }
+                String result = Result.organizingContainers(container);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
+        });
 
-            String result = organizingContainers(container);
-
-            bufferedWriter.write(result);
-            bufferedWriter.newLine();
-        }
-
+        bufferedReader.close();
         bufferedWriter.close();
-
-        scanner.close();
     }
 }
