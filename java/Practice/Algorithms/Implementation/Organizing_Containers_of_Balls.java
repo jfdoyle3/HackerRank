e322a854b3a64b64e328aa05fb133e713efc161e
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-04-27 21:51:59 -0400

    removed comments

--- java/Practice/Algorithms/Implementation/Organizing_Containers_of_Balls.java
+++ java/Practice/Algorithms/Implementation/Organizing_Containers_of_Balls.java
@@ -12,13 +12,6 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'organizingContainers' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts 2D_INTEGER_ARRAY container as parameter.
-     */
-
     public static String organizingContainers(List<List<Integer>> container) {
         String result="";
         
