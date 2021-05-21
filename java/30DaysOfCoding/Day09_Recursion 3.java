c053160da4d5506896830bca010297978b83a684
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-05-20 21:40:05 -0400

    refactored recursion 3

--- java/30DaysOfCoding/Day09_Recursion 3.java
+++ java/30DaysOfCoding/Day09_Recursion 3.java
@@ -12,8 +12,8 @@ public class Solution {
     static int factorial(int n) {
         if (n <= 1)
             return 1;
-           else
-           return factorial(n - 1) * n;
+   
+        return factorial(n - 1) * n;
 
     }
 
