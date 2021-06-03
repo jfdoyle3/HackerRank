dbf6dda4cc0386fba2e5b744783f1315f177091f
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-06-02 21:08:33 -0400

    Started Bonetrousle

---
+++ java/Practice/Algorithms/Constructive Algorithms/Bonetrousle.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'bonetrousle' function below.
+     *
+     * The function is expected to return a LONG_INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. LONG_INTEGER n
+     *  2. LONG_INTEGER k
+     *  3. INTEGER b
+     */
+
+    public static List<Long> bonetrousle(long n, long k, int b) {
+        
+    
+    return null;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                long n = Long.parseLong(firstMultipleInput[0]);
+
+                long k = Long.parseLong(firstMultipleInput[1]);
+
+                int b = Integer.parseInt(firstMultipleInput[2]);
+
+                List<Long> result = Result.bonetrousle(n, k, b);
+
+                bufferedWriter.write(
+                    result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                    + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
