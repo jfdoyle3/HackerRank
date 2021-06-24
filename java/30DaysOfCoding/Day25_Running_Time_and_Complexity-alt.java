0c62db3c82ddacf8268839c43ed7f2e04d22c3b5
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-06-23 22:45:51 -0400

    working on Day25_Running_Time_and_Complexity-alt

--- java/30DaysOfCoding/Day25_Running_Time_and_Complexity-alt.java
+++ java/30DaysOfCoding/Day25_Running_Time_and_Complexity-alt.java
@@ -4,8 +4,6 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-       // Boolean isPrime=true;
-       int counter=1;
        
        // Initialize Scanner for input
         Scanner input=new Scanner(System.in);
@@ -19,31 +17,47 @@ public class Solution {
             // input test case numbers
             int testNum=input.nextInt();
             
-            //take the square root of number
-            double sqrtNum=Math.sqrt(testNum);
-             
-           // System.out.println(sqrtNum);
-           
-             // loop from highest of square root to lowest
-             for(double i=sqrtNum-1; i>=2; i--){
-                 
-                // if square root  modulo of number equals then it's prime
-                if(sqrtNum%i==0){
-                //    System.out.println(i+"  Prime");
-                    counter=1;
-                      
-                }
-                // if square root modulo of number is greater than zero it's not prime   
-                if(sqrtNum%i>0) {
-                //    System.out.println(i+"  Not Prime");
-                  //  isPrime=false;
-                    counter++;
-                }
-               // System.out.printf("I:  %f\tsr: %f\n",i,sqrtNum%i);
-             } 
-           System.out.println(counter);     
-            String results=(counter==2) ? "Prime" : "Not prime";
-            System.out.println(results);
+            // Run check on method checkForPrime to determine prime or not
+            String isPrime=(checkForPrime(testNum)) ? "Prime" : "Not prime";
+            
+            // output Prime or Not prime
+            System.out.println(isPrime);
             }
         }
+        static boolean checkForPrime(double number){
+            // Return true for Prime numbers
+            if(number==2 || number==3 || number==5 || number==7)
+                return true;
+            // 1 is not prime return false
+            if(number==1)
+                return false;
+           // for (int i = 0; i < 5; i++) {
+			// take the square root of number
+			//double sqrtNum = Math.sqrt(number);
+			//number = sqrtNum;
+			//System.out.println("sqr:  " + number);
+			//if(number==3)
+			//	System.out.println("Not prime");
+			//else
+			//	System.out.println("Prime");
+				
+		}
+            // looping
+            while(true){
+             
+                // take the square root of number
+            double sqrtNum=Math.sqrt(number);
+            number=sqrtNum;    
+                System.out.println("sqr:  "+number);
+
+if (number==2 || number==3 || number==5 || number==7)
+                return true;
+            else
+                break;
+            }
+            
+           return false;
+        }
     }
+
+  h
