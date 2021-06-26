e363f4307964fcd045dc319a1caf0647b7183761
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-06-25 23:45:46 -0400

    working on 03_Java_Substring_Comparisons

--- java/30DaysOfCoding/Day27_Testing.java
+++ java/30DaysOfCoding/Day27_Testing.java
@@ -1,48 +1,48 @@
 import java.util.*;
 
-public class Solution {
+	public class Solution {
 
-    public static int minimum_index(int[] seq) {
-        if (seq.length == 0) {
-            throw new IllegalArgumentException("Cannot get the minimum value index from an empty sequence");
-        }
-        int min_idx = 0;
-        for (int i = 1; i < seq.length; ++i) {
-            if (seq[i] < seq[min_idx]) {
-                min_idx = i;
-            }
-        }
-        return min_idx;
-    }
+		public static int minimum_index(int[] seq) {
+			if (seq.length == 0) {
+				throw new IllegalArgumentException("Cannot get the minimum value index from an empty sequence");
+			}
+			int min_idx = 0;
+			for (int i = 1; i < seq.length; ++i) {
+				if (seq[i] < seq[min_idx]) {
+					min_idx = i;
+				}
+			}
+			return min_idx;
+		}
 
-    static class TestDataEmptyArray {
-        public static int[] get_array() {
-            int[] arr={};
-            return arr;
-        }
-    }
+		static class TestDataEmptyArray {
+			public static int[] get_array() {
+				int[] arr={};
+				return arr;
+			}
+		}
 
-    static class TestDataUniqueValues {
-        public static int[] get_array() {
-            int[] arr={4,7,5,1};
-            return arr;
-        }
+		static class TestDataUniqueValues {
+			public static int[] get_array() {
+				int[] arr={4,7,5,1};
+				return arr;
+			}
 
-        public static int get_expected_result() {
-            return 3;
-        }
-    }
+			public static int get_expected_result() {
+				return 3;
+			}
+		}
 
-    static class TestDataExactlyTwoDifferentMinimums {
-        public static int[] get_array() {
-            int[] arr={1,1,3};
-            return arr;
-        }
+		static class TestDataExactlyTwoDifferentMinimums {
+			public static int[] get_array() {
+				int[] arr={1,1,3};
+				return arr;
+			}
 
-        public static int get_expected_result() {
-            return 0;
-        }
-    }
+			public static int get_expected_result() {
+				return 0;
+			}
+		}
 
     
 	public static void TestWithEmptyArray() {
---
+++ java/Practice/Java/Strings/03_Java_Substring_Comparisons.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "a";
+        String largest = "b";
+        String[] arr=new String[s.length()-2];
+       System.out.println("len: "+s.length());
+       System.out.println("len/k "+s.length()/k);
+        
+        int strLen=s.length()-2;
+       
+         System.out.println(strLen);
+
+         for(int idx=0; idx<strLen; idx++){
+            
+            String x=s.substring(idx,idx+k);
+         //   System.out.println(x);
+            arr[idx]=x;
+        //     smallest=x;
+        //     if(smallest.compareTo(largest)>0)
+        //         largest=x;
+        //   // if(smallest.compareTo(largest)<0)
+       //    System.out.printf("%s = %d\n",x,smallest.compareTo(largest));
+            
+        }
+      
+        for(String word : arr)
+            System.out.println("l "+word);
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        
+        return smallest + "\n" + largest;
+    }
+
+
