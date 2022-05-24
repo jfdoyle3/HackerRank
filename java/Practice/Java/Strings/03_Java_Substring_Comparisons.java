051a9ec93f2045bd9b8dbc1b19143e5e873c13f2
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-05-24 16:48:02 -0400

    Completed Java SubString Comparisons and Anagrams

--- java/Practice/Java/Strings/03_Java_Substring_Comparisons.java
+++ java/Practice/Java/Strings/03_Java_Substring_Comparisons.java
@@ -2,42 +2,19 @@ import java.util.Scanner;
 
 public class Solution {
 
-    public static String getSmallestAndLargest(String s, int k) {
-        String smallest = "a";
-        String largest = "b";
-        String[] arr=new String[s.length()-2];
-    //    System.out.println("len: "+s.length());
-    //    System.out.println("len/k "+s.length()/k);
+   public static String getSmallestAndLargest(String s, int k) {
+        List<String> subStrings=new ArrayList<>();
+        int truncateLength=k-1;
+        String[] arr=new String[s.length()-truncateLength];
         
-        int strLen=s.length()-2;
-       
-     //    System.out.println(strLen);
-
-        for(int idx=0; idx<strLen; idx++)
+        int strLen=s.length()-truncateLength;
+        for(int idx=0; idx<strLen; idx++){
             arr[idx]=s.substring(idx,idx+k);
-            
-               
-       //    System.out.printf("%s = %d\n",x,smallest.compareTo(largest));
-            
-
-        for(int idx=0; idx<arr.length-1; idx++){
-           for(int idx2=1; idx2<arr.length; idx2++){
-               if(arr[idx].compareTo(arr[idx2])>0)
-                        largest=arr[idx];
-           }
-        }
+            subStrings.add(arr[idx]);
+        }     
+        Collections.sort(subStrings);
         
-        // for(String word : arr){
-        //if(smallest.compareTo(largest)>0)
-        //         largest=x;
-        // if(smallest.compareTo(largest)<0)
-            // System.out.println(word);
-        
-        // Complete the function
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
-        
-        return smallest + "\n" + largest;
+        return subStrings.get(0) + "\n" + subStrings.get(subStrings.size()-1);
     }
 
 
