dcd5b87d45d9268fbd10923a7c0a72c8d95d32a7
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-06-22 06:12:02 -0400

    completed Day 1 & 2 30 Days in Ruby

---
+++ ruby/30-days/Day01_Data_Types.rb
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+number=gets.to_i
+decimal=gets.to_f
+sentence=gets
+# Print the sum of both integer variables on a new line.
+puts number+i
+# Print the sum of the double variables on a new line.
+puts decimal+d
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+puts s+sentence
