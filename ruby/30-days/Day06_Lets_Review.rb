787a1805e84fbd4999e0c9bf183ee64543be0cb7
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-06-24 16:43:41 -0400

    Working on Day06_Lets_Review

---
+++ ruby/30-days/Day06_Lets_Review.rb
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __FILE__ ==$0
+    n=gets.chomp.to_i
+    puts n
+    for i in 0..n
+        s=gets.chomp
+        puts s
+        
+    end
+end
