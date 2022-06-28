21d55299de6269ae60516d7859602506dfce970b
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-06-28 06:10:35 -0400

    Still working on Day06_Lets_Review

--- ruby/30-days/Day06_Lets_Review.rb
+++ ruby/30-days/Day06_Lets_Review.rb
@@ -1,26 +1,26 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 if __FILE__ ==$0
-    test_case=gets.chomp.to_i
-    for test in 0..test_case
+    test_cases=gets.chomp().to_i
+    test_cases.times do |test_cases|
         word=gets.chomp()
-		#word_array=word.split("")
-        #print word_array
-        if i%2==0
-            len=s.length()
-            len.times do |len|
-             if len%2==0
-                 print s[len]
-             end
-            end    
-        end
-        print " "
-        if i%2!=0
-           len1=s.length()
-           len1.times do |len1|
-            if len1%2!=0
-                print s[len1] 
+        word_array=word.split("")
+        
+        word_array.length().times do |index|
+           # puts word_array[index] + " "+index.to_s
+            if index%2==0
+                print word_array[index]
+                end    
             end
-           end 
-        end
+        
+
+        # print " "
+        # if test%2!=0
+        #    len1=s.length()
+        #    len1.times do |len1|
+        #     if len1%2!=0
+        #         print s[len1] 
+        #     end
+        #    end 
+        # end
     end
 end
