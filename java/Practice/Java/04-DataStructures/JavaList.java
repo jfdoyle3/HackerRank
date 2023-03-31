8cb6369d4869b635f267ae6b16a5c646df4eaa6a
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-03-30 21:50:48 -0400

    Worked on Java List Algorithm

---
+++ java/Practice/Java/04-DataStructures/JavaList.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        List<Integer> list=new ArrayList<>();
+        int listSize=scan.nextInt();
+         
+         // create the list.
+        for(int idx=0; idx<listSize; idx++)
+            list.add(scan.nextInt());
+        
+        // Get Query
+        int queries=scan.nextInt();
+        // System.out.println("q|"+queries);
+        for(int qry=0; qry<queries; qry++){
+            String query=scan.next();
+            if(query.equals("Insert")){
+                int index=scan.nextInt();
+                int number=scan.nextInt();
+                if(index>=list.size()){
+                    list.add(number);
+                }else{
+                    list.set(index,number);
+                    }
+                }
+            if(query.equals("Delete")){
+                int index=scan.nextInt();
+                list.remove(index);
+            }
+        }
+         
+        for(Integer num : list)
+            System.out.print(num+" ");
+            
+         scan.close(); 
+    }
+}
